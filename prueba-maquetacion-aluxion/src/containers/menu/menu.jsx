45bb04d26f57b168a81294c0015644aaf14c5941
Mateoo-pierre <minomateo02@gmail.com>
2022-03-08 09:34:59 -0300

    hover li menu

--- prueba-maquetacion-aluxion/src/containers/menu/menu.jsx
+++ prueba-maquetacion-aluxion/src/containers/menu/menu.jsx
@@ -8,13 +8,16 @@ export const Menu = () => {
         <>  
             <div className="menu">
                 <ul className="ul">
-                    <li className="li1 li"><a href="#">Collection</a></li>
-                    <div className="bg1">
-                        <ul>
-                            <li>Furniture</li>
-                            <li>Lighting</li>
-                            <li>Accessories</li>
-                        </ul>
+                    <div className="collect-container">
+                        <li className="li1 li"><a href="#">Collection</a></li>
+                        <div className="bg1">
+                            <ul>
+                                <li className="li-furniture">Furniture</li>
+                                <div className="bg1-forniture"></div>
+                                <li>Lighting</li>
+                                <li>Accessories</li>
+                            </ul>
+                        </div>
                     </div>
                     <li className="li2 li"><a href="#">Desing</a></li>
                     <li className="li3 li"><a href="#">Craftmanship</a></li>
